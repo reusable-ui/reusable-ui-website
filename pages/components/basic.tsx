1feae9e51ff8ef3ddb81e413dd9a89646bf6de59
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-05 05:44:24 +0700

    updated basic

--- pages/components/basic.tsx
+++ pages/components/basic.tsx
@@ -6,13 +6,18 @@ import { basic } from '../../packages/packageList'
 import * as packages from '../../packages/packageList'
 import { SizeProperty, VariantProperties } from '../../properties/sections/variantProperties'
 import { Preview } from '../../components/Preview'
+import { Basic as OriBasic, BasicProps } from '@reusable-ui/components'
+
+
+
+const Basic = (props: BasicProps) => <OriBasic {...props} theme={props.theme ?? 'primary'} />
 
 
 
 const BasicPage: NextPage = () => {
     return (<>
         <Head>
-            <title>{`${basic.componentTag} Components`}</title>
+            <title>{`${basic.componentTag} Component`}</title>
             <meta name="description" content={`${basic.componentTag} is a simple box layout component with built-in variants: ${packages.resizable.packageShortName}, ${packages.themable.packageShortName}, ${packages.gradientable.packageShortName}, ${packages.outlineable.packageShortName}, ${packages.mildable.packageShortName}, and ${packages.nudible.packageShortName}`} />
         </Head>
         <Section title={<>{basic.packageDisplay} Component</>}>
@@ -22,8 +27,13 @@ const BasicPage: NextPage = () => {
             <VariantProperties>
                 <SizeProperty>
                     <Preview>
-                        <p>blah...</p>
-                        <p>blah...</p>
+                        {['sm', undefined, 'lg'].map((sizeName, index) =>
+                            <Basic
+                                key={index}
+                            >
+                                A {'<Basic>'} with {sizeName ?? 'default'} size
+                            </Basic>
+                        )}
                     </Preview>
                 </SizeProperty>
             </VariantProperties>
