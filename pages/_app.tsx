a190fabc5349ca7427f39c646263541d17ed0d29
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-12 05:23:07 +0700

    updated google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ const Header = () => {
     return (
         <>
             <Script strategy="lazyOnload" async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
-            <Script strategy="lazyOnload">{
+            <Script strategy="lazyOnload" id='g-analytics'>{
 `
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
