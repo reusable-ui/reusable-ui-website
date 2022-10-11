6971141ac87934e7102c6a6d2e56412decd29645
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-12 00:33:38 +0700

    updated home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import Image from 'next/image'
 import styles from '../styles/Home.module.scss'
-import { GenericSection } from '../components/Section'
+import { HeroSection, Main, Section } from '../components/Section'
 import { ButtonIcon as Button } from '@reusable-ui/components'
 import { ExtLink } from '../components/ExtLink'
 
@@ -16,8 +15,8 @@ const Home: NextPage = () => {
             <meta name="description" content="Reusable-UI - A React component library for building sites quickly. Reuse the components, save your time now and later." />
         </Head>
         
-        <GenericSection theme='primary'>
-            <article className={styles.hero}>
+        <Main>
+            <HeroSection theme='primary' className={styles.hero}>
                 <h1 className='display-4'>Reusable-UI</h1>
                 <p className='display-6'>Reuse the components,<br />save your time now and next</p>
                 <div className='actions'>
@@ -25,44 +24,36 @@ const Home: NextPage = () => {
                         <Link href='/guide-create-simple-app'>Get Started</Link>
                     </Button>
                 </div>
-            </article>
-        </GenericSection>
-        <GenericSection>
-            <article className={styles.features}>
-                <section>
-                    <h3 className='h5'>Reusability</h3>
+            </HeroSection>
+            <Section className={styles.features}>
+                <Section title='Reusability'>
                     <p>Our primary goal is reuse our/your components. We save our time together.</p>
                     <p>By modularizing the (sub) components and (sub) stylesheets, the chance of reusability increases.</p>
-                </section>
-                <section>
-                    <h3 className='h5'>Customizable</h3>
+                </Section>
+                <Section title='Customizable'>
                     <p>We expose APIs for customizing each component.</p>
                     <p>The component configuration is just a collection of <em>css variable</em>.<br />
                         No compilation needed for applying the changes, <em>instantly updated!</em>
                     </p>
-                </section>
-                <section>
-                    <h3 className='h5'>Themable</h3>
+                </Section>
+                <Section title='Themable'>
                     <p>All our component have a shared configuration via <em>css variables</em>.</p>
                     <p>Edit the theme in one place, all the components will be synched in harmoniously.</p>
-                </section>
-                <section>
-                    <h3 className='h5'>Intellisense Support</h3>
+                </Section>
+                <Section title='Intellisense Support'>
                     <p>Written in TypeScript (superset of JavaScript).</p>
                     <p>Supports autocomplete in popular code editor like VS Code.</p>
-                </section>
-                <section>
-                    <h3 className='h5'>Open Source</h3>
+                </Section>
+                <Section title='Open Source'>
                     <p>You can freely to use, modify or re-publish your with your own components.</p>
                     <p>All our codes are available on <ExtLink dofollow={true} href='https://github.com/reusable-ui'>GitHub</ExtLink>.</p>
-                </section>
-                <section>
-                    <h3 className='h5'>Look This Site!</h3>
+                </Section>
+                <Section title='Look This Site!'>
                     <p>This site is made from our <strong>Reusable-UI</strong> components!</p>
                     <p>Built on top NextJS + Reusable-UI.</p>
-                </section>
-            </article>
-        </GenericSection>
+                </Section>
+            </Section>
+        </Main>
     </>);
 }
 export default Home
