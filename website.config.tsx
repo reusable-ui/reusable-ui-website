b7ebc86f6cca58d32d8c8475bb3c90b4177986f9
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-10-13 02:37:23 +0700

    updated <code>

--- website.config.tsx
+++ website.config.tsx
@@ -1,5 +1,7 @@
-import { styleSheet, cssVars, style, rule, atGlobal, atRoot, vars, descendants } from '@cssfn/core'
-import { iconConfig } from '@reusable-ui/components'
+import { styleSheet, cssVars, style, rule, atGlobal, atRoot, vars, descendants, switchOf } from '@cssfn/core'
+import { basics, iconConfig } from '@reusable-ui/components'
+import { codes, colors, usesMildable, usesOutlineable } from '@reusable-ui/core'
+
 
 
 interface SiteVars {
@@ -26,6 +28,44 @@ interface SiteVars {
 export const [siteVars] = cssVars<SiteVars>({ minify: false });
 
 
+interface CodeVars {
+    foregFn    : any
+    altForegFn : any
+    foreg      : any
+}
+const [codeVars] = cssVars<CodeVars>();
+
+const codeStyle = () => {
+    const { outlineableVars } = usesOutlineable();
+    const { mildableVars } = usesMildable();
+    
+    return style({
+        ...rule('a:has(code:only-child)', {
+            textDecoration: [['none'], '!important'], // disable underline for <a> wrapping single <code>
+        }),
+        ...rule(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', '.h1', '.h2', '.h3', '.h4', '.h5', '.h6'], {
+            ...descendants('code', {
+                fontSize: [['inherit'], '!important'],
+            }),
+        }),
+        ...rule('code', {
+            ...vars({
+                [codeVars.foregFn   ] : [[
+                    switchOf(outlineableVars.outlinedSw, mildableVars.mildSw), // the (outlined|mild) switching function
+                    codes.foreg,
+                ]],
+                [codeVars.altForegFn] : colors.dangerMild,
+                [codeVars.foreg     ] : switchOf(
+                    codeVars.foregFn,
+                    codeVars.altForegFn,
+                ),
+            }),
+            color      : codeVars.foreg,
+            transition : basics.transition,
+        }, { specificityWeight: 2 }),
+    });
+};
+
 styleSheet(() => style({
     ...atGlobal({
         // ...rule('*', {
@@ -45,14 +85,7 @@ styleSheet(() => style({
             position        : 'sticky',
             insetBlockStart : '0px',
         }),
-        ...rule('a:has(code:only-child)', {
-            textDecoration: [['none'], '!important'],
-        }),
-        ...rule(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', '.h1', '.h2', '.h3', '.h4', '.h5', '.h6'], {
-            ...descendants(['code', '.code'], {
-                fontSize: [['inherit'], '!important'],
-            }),
-        }),
+        ...codeStyle(),
         
         ...atRoot([
             vars({
