cf914457f9f8b6ff1f5215a2dc594f1de5c9f83d
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-12-03 16:30:01 +0700

    added sitemap.xml & robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://reusableui.org',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    generateIndexSitemap: false,
+    changefreq: 'weekly',
+}
