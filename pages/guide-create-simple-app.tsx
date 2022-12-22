eaba73491fe591887012f38cf5eb3f89bfeb3f79
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-12-22 17:26:16 +0700

    added guide

---
+++ pages/guide-create-simple-app.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import React from 'react'
+import { Main, Section } from '../components/Section'
+
+
+
+const GuideCreateSimpleApp: NextPage = () => {
+    return (<>
+        <Head>
+            <title>Create Simple App using Reusable-UI</title>
+            <meta name="description" content="Coming soon." />
+        </Head>
+        
+        <Main title='Create Simple App using Reusable-UI'>
+            <p>
+                Coming soon! Please come back later.
+            </p>
+            <Section title='Foo Section'>
+                <p>
+                    Under construction.
+                </p>
+            </Section>
+        </Main>
+    </>);
+}
+export default GuideCreateSimpleApp
