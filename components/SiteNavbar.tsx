563fa46d09f1790dac6bc414dd277d68c81838d4
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-12-22 18:40:39 +0700

    fixed scrolling problem on load

--- components/SiteNavbar.tsx
+++ components/SiteNavbar.tsx
@@ -5,14 +5,28 @@ import SiteNavbarMenu from './SiteNavbarMenu';
 
 
 
-if (typeof(document) !== 'undefined') {
+//#region reset scrollbar to the top on the first full page load
+if (typeof(window) !== 'undefined') { // run on client_side only, do nothing on server_side
     const scrollElm = document.scrollingElement;
     if (scrollElm) {
-        requestAnimationFrame(() => {
+        scrollElm.scrollTop = 0;
+        
+        
+        
+        document.addEventListener('scroll', () => {
             scrollElm.scrollTop = 0;
-        });
+        }, { once: true, passive: true }); // run *once* to prevent unpleasant accessibility in case of the 'DOMContentLoaded' is *never* triggered
+        
+        
+        
+        window.addEventListener('DOMContentLoaded', () => {
+            requestAnimationFrame(() => {
+                scrollElm.scrollTop = 0;
+            });
+        }, { once: true });
     } // if
 } // if
+//#endregion reset scrollbar to the top on the first full page load
 
 
 
