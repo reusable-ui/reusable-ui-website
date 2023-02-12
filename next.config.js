c9c1dfd6d51bb9805f20f1ff3459836514118498
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-02-12 15:40:44 +0700

    updated cssfn deps

--- next.config.js
+++ next.config.js
@@ -12,9 +12,10 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig;
 
 // const withBundleAnalyzer = require('@next/bundle-analyzer')({
 //   enabled: true,
 // });
 // module.exports = withBundleAnalyzer(nextConfig);
+
+module.exports = nextConfig;
